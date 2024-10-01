1973656daf0d90f94298b11607ef8b2d95f83c60
Júlia Sales Novais <jsn@cesar.school>
2024-10-01 12:35:03 -0300

    começo da revisao

--- 3 SM/AED/Revisao/main.c
+++ 3 SM/AED/Revisao/main.c
@@ -1,9 +1,26 @@
 #include <stdio.h>
+#include <stdlib.h>
 
 typedef struct Node {
     int num;
-    struct Node next;
-};Node
+    struct Node *next;
+}Node;
+
+void enqueue(Node **head, Node **tail, int n) {
+    Node *novo = (Node *)malloc(sizeof(Node));
+    if (novo != NULL) {
+        novo->num= n;
+        novo->next=NULL;
+    }
+    if (*head==NULL) {
+        *head=novo;
+        *tail=novo;
+    }else{
+        (*tail)->next=NULL;
+        *tail=novo;
+    }
+}
+
 int main(void)
 {
     printf("Hello, World!\n");
